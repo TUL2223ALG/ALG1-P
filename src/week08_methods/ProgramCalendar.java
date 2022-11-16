0152d80554dc3432ab6dc2efbe72242eca1fcdd9
jana vitvarova <jana.vitvarova@tul.cz>
2022-11-16 11:04:51 +0100

    Add zeller

--- src/week08_methods/ProgramCalendar.java
+++ src/week08_methods/ProgramCalendar.java
@@ -6,12 +6,28 @@ public class ProgramCalendar {
     static Scanner sc = new Scanner(System.in);
     
     public static void main(String[] args) {
-        System.out.println("Zadej rok:");
-        int y = sc.nextInt();
+        System.out.println("Zadej den, mesic a rok: ");
+        int day = sc.nextInt();
+        int month = sc.nextInt();
+        int year = sc.nextInt();
         
-        boolean result = DateTimeTools.isLeap(y);
+        int dayInWeek= DateTimeTools.zeller(day, month, year); 
+
+        String dayInWeekText = null;
+        switch (dayInWeek)
+        {
+            case 1 -> dayInWeekText = "Monday";
+            case 2 -> dayInWeekText = "Tuesday";
+            case 3 -> dayInWeekText = "Wednesday";
+            case 4 -> dayInWeekText = "Thursday";
+            case 5 -> dayInWeekText = "Friday";
+            case 6 -> dayInWeekText = "Saturday";
+            case 7 -> dayInWeekText = "Sunday";
+        } 
         
-        System.out.println("zadany rok " + (result?"je":"neni") + " prestupny");
+        System.out.println("Zadany datum je " + dayInWeekText + ".");
+        
+        boolean result = DateTimeTools.isLeap(year);
+        System.out.println("Zadany rok " + (result?"je":"neni") + " prestupny.");
     }
-    
 }
