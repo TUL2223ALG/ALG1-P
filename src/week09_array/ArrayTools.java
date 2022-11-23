8e43e56474f90f4f3eadf904b9fe44dbe07f8792
jana vitvarova <jana.vitvarova@tul.cz>
2022-11-23 10:55:10 +0100

    More array methods

--- src/week09_array/ArrayTools.java
+++ src/week09_array/ArrayTools.java
@@ -22,7 +22,92 @@ public final class ArrayTools {
         }
         return maxValue;
     }
+    /**
+     * 
+     * @param array
+     * @return index of maximum or -1 if array is empty
+     */
+    public static int maxvalueIndexFirst(int[] array){
+        if(array == null || array.length < 1 ){ //osetreni limitniho stavu 
+            //pole bez alokovane pameti, nebo prazdne pole
+            return -1;
+        }
+        int max = array[0];
+        int pozMax = 0;
+        for (int i = 0; i < array.length; i++) {
+                if (array[i] > max) {
+                    max = array[i];
+                    pozMax = i;
+                }
+        }
+        return pozMax;
+    }
     
+    public static int maxvalueIndexLast(int[] array){
+        int max = array[0];
+        int pozMax = 0;
+        for (int i = 0; i < array.length; i++) {
+                if (array[i] >= max) {
+                    max = array[i];
+                    pozMax = i;
+                }
+        }
+        return pozMax;
+    }
+    
+    public static int maxvalueIndexLast1(int[] array){
+        int max = array[0];
+        int pozMax = 0;
+        for (int i = array.length - 1; i >= 0; i--) { //prochazeni pole odzadu
+                if (array[i] > max) {
+                    max = array[i];
+                    pozMax = i;
+                }
+        }
+        return pozMax;
+    }
+    
+    public static int[] maxMin(int[] array, boolean findFirst) {
+        int max = array[0];
+        int min = array[0];
+        int pozMax = 0;
+        int pozMin = 0;
+        if (findFirst) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] > max) {
+                    max = array[i];
+                    pozMax = i;
+                }
+                if (array[i] < min) {
+                    min = array[i];
+                    pozMin = i;
+                }
+            }
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] >= max) {
+                    max = array[i];
+                    pozMax = i;
+                }
+                if (array[i] <= min) {
+                    min = array[i];
+                    pozMin = i;
+                }
+            }
+        }
+        return new int[]{max, pozMax, min, pozMin};
+    }
+    
+    public static int firstIndex(int[] array, int number) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == number) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+                   
     public static void init(int[] a){
         for (int i = 0; i < a.length; i++) {
             a[i] = 0;
@@ -45,22 +130,36 @@ public final class ArrayTools {
         return a;
     }
     
+    public static void reverse(int[] a){
+        int temp;
+        for (int i = 0; i < a.length/2; i++) {
+            temp = a[a.length -1 -i];
+            a[a.length -1 -i] = a[i];
+            a[i] = temp;
+        }
+    }
+    
     //testing main
     public static void main(String[] args) {
-        int [] myArray = {2,3,4,8}; //staticka inicializace konkretnimi hodnotami
-        int sum = sum(myArray);
-        System.out.println("Suma " + sum); //expected 17
-        System.out.println("Max hodnota " + maxValue(myArray)); //expected 8
-        init(myArray);
+        int [] myArray = {1,8,6,2}; //staticka inicializace konkretnimi hodnotami
         UI.display(myArray);
-        
-        System.out.println("Pole nahodnych cisel");
-        int[] b =  new int[10];
-        random(b, -5, -1);
-        UI.display(b);
-        
-        int n = 10;
-        int[] d = random2(n, 10, 20);
-        UI.display(d);
+        reverse(myArray);
+        UI.display(myArray);
+        //int maxvalueIndexFirst = maxvalueIndexFirst(myArray);
+        //System.out.println(maxvalueIndexFirst);
+//        int sum = sum(myArray);
+//        System.out.println("Suma " + sum); //expected 17
+//        System.out.println("Max hodnota " + maxValue(myArray)); //expected 8
+//        init(myArray);
+//        UI.display(myArray);
+//        
+//        System.out.println("Pole nahodnych cisel");
+//        int[] b =  new int[10];
+//        random(b, -5, -1);
+//        UI.display(b);
+//        
+//        int n = 10;
+//        int[] d = random2(n, 10, 20);
+//        UI.display(d);
     }
 }
