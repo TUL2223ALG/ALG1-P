5082ae158f40ef2767a952b2072515da418be147
jana vitvarova <jana.vitvarova@tul.cz>
2022-12-07 10:57:12 +0100

    More 2D

--- src/week10_2Darray/MatrixTools.java
+++ src/week10_2Darray/MatrixTools.java
@@ -57,8 +57,48 @@ public class MatrixTools {
         }
         return maxValue;
     }
-   
     
+    //to check
+    public static boolean isStochastic (double [][] matrix){
+    	double rowSum;
+    	for (int i = 0; i < matrix.length; i++) {
+        	rowSum=0;
+        	for (int j = 0; j < matrix[i].length; j++) {
+                    if (matrix[i][j]>=0){
+                	rowSum+=matrix[i][j];
+                    }   
+                    else{
+                	return false;
+}   
+        	}
+            if (rowSum!=1) {
+           	return false;
+            }
+    	}
+    	return true;
+    }
+    
+    //pouzitelne i kdyz to nebude metoda (nevyuziva return false/true)
+    public static boolean isStochastic1 (double [][] matrix){
+    	double rowSum;
+    	boolean isStochastic = true;
+    	for (int i = 0; i < matrix.length && isStochastic; i++) {
+        	rowSum=0;
+        	for (int j = 0; j < matrix[i].length && isStochastic; j++) {
+                    if (matrix[i][j]>=0){
+                	rowSum+=matrix[i][j];
+                    }   
+                    else{
+                	isStochastic = false;
+}   
+        	}
+            if (rowSum!=1) {
+           	isStochastic = false;
+            }
+    	}
+    	return isStochastic;
+    }
+   
         //testing
     public static void main(String[] args) {
         int[][] my = fill(4, 5, 10);
